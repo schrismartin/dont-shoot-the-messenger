d45b0957370d445a3b3254302e1510d58251b36e
Chris Martin <schrismartin@me.com>
2016-11-12 22:16:43 -0600

    Pre-merge commit

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ let package = Package(
         Target(name: "App", dependencies: ["Library"]),
     ],
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/OpenKitten/MongoKitten.git", majorVersion: 2, minor: 0)
     ],
     exclude: [
         "Config",
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,14 +1,41 @@
 import Vapor
 import Library
+import MongoKitten
 
 let drop = Droplet()
 
-drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+
+
+
+
+do {
+    let mongoServer = try Server(hostname: "mongodb://master:dontshoot@ds151707.mlab.com:51707/dont-shoot-the-messenger")
+	try mongoServer.connect()
+	let dontshoot = mongoServer["dontshoot"]
+	let users = dontshoot["users"]
+    
+    // Define Routes
+    
+    drop.get("/") { req in
+        do {
+            guard let userDocument = try users.findOne(matching: "_id" == ObjectId("thisndgs")) else {
+                return "I don’t know you.."
+            }
+
+        } catch {
+            return "there was a problem getting with users.findOne"
+        }
+        
+        return "Made it to end without returning something"
+    }
+    
+    // Do some other stuff
+    drop.resource("posts", PostController())
+
+    // Run it
+	drop.run()
+} catch {
+	print("Cannot connect to MongoDB")
 }
 
-drop.resource("posts", PostController())
 
-drop.run()
